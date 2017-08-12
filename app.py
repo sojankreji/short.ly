9ba0023443727ee76070571b421560f3832078f6
Délita Makanda <delita.makanda@gmail.com>
2017-08-12 19:04:59 +0200

    add page

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from django.conf.urls import url
+from django.http import HttpResponse
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'uq)b=evycs1+=*3!f0c%3h%(j@_=h%c+=i6363mntz5z)_)k*0'
 
@@ -6,4 +9,26 @@ DEBUG = True
 
 ROOT_URLCONF = __name__
 
-urlpatterns = []
+def home(request):
+    color = request.GET.get('color', 'red')
+    return HttpResponse('<h1 style="color:' + color + '">Welcome to the Tinyapp\'s Homepage!</h1>')
+
+def about(request):
+    title = 'App'
+    author = 'Délita Makanda'
+    html = '''<!DOCTYPE html>
+    <html>
+    <head>
+      <title>''' + title + '''</title>
+    </head>
+    <body>
+        <h1>About ''' + title + '''</h1>
+        <p>This Website was developed by ''' + author + '''.</p>
+    </body>
+    </html>'''
+    return HttpResponse(html)
+
+urlpatterns = [
+    url(r'^$', home),
+    url(r'^about/$', about),
+]
