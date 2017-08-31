94cadd80a49af6c0e5733149fd30b6726b5dd225
Délita Makanda <delita.makanda@gmail.com>
2017-08-31 20:20:58 +0200

    add user

--- shortly/settings_production.py
+++ shortly/settings_production.py
@@ -9,7 +9,7 @@ DATABASES = {
   'default': {
       'ENGINE': 'django.db.backends.postgresql_psycopg2',
       'NAME': 'shortly',
-      'USER': 'postgres',
+      'USER': 'u_shortly',
       'PASSWORD': '123',
       'HOST': 'localhost',
       'PORT': '',
